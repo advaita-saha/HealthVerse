c26d89f0a476967a203ca66284ce6edd14bd32e1
Advaita Saha <adv11sphs@gmail.com>
2022-03-24 03:48:28 +0530

    contract optimized, gas fee reduced, made faster, the storage system of manager changed

--- contracts/BlockHealth.sol
+++ contracts/BlockHealth.sol
@@ -1,33 +1,30 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.4;
 
 contract BlockHealth {
 
+    // Declaring the public variables to store basic info about the contract
     string public aboutUs = "Secure decentralized electronic health records sharing system based on blockchains";
     address public owner;
-    address[] public managers;
+    mapping(address => bool) managers;
     
+    // Declaring the private variables storing the data of the patients mapped with the doctors
     mapping(address => address[]) patientList;
     mapping(address => string[]) medicalRecords;
 
+    // Modifier to give access only to the owner of the contract
     modifier onlyOwner(){
         require(msg.sender == owner, "Only the owner has access to this");
         _;
     }
 
+    // Modifier to only give access to Managers and the Owner of the contract
     modifier restricted(){
-        bool allowed = false;
-        for(uint i; i < managers.length; i++){
-            if(managers[i] == msg.sender){
-                allowed = true;
-                break;
-            }
-        }
-        require(allowed || owner == msg.sender, "Only allowed managers or the owner can perform this operation");
+        require(managers[msg.sender] || owner == msg.sender, "Only allowed managers or the owner can perform this operation");
         _;
     }
 
+    // Modifier that only allows patients assigned doctor to see reports
     modifier onlyDoctor(address patient){
         bool allowed = false;
         for(uint i; i < patientList[msg.sender].length; i++){
@@ -41,9 +38,11 @@ contract BlockHealth {
     }
 
     constructor(){
+        // Setting the owner of the contract as who deploys it
         owner = msg.sender;
     }
 
+    // Function to add a Medical Report to the database
     function addRecord(address patient, address doctor, string memory url) public restricted {
         bool isThere = false;
         for(uint i; i < patientList[doctor].length; i++){
@@ -60,29 +59,34 @@ contract BlockHealth {
 
     }
 
+    // Function to get the list of patients of a doctor
     function getPatients(address doctor) public view restricted returns(address[] memory){
         return patientList[doctor];
     }
 
+    // Function to get the list of my patients ( function which can be called from the doctors account )
+    function getMyPatients() public view returns(address[] memory){
+        return patientList[msg.sender];
+    }
+
+    // Function to get all the medical reports for the doctor to see
     function getRecords(address patient) public view onlyDoctor(patient) returns(string[] memory){
         return medicalRecords[patient];
     }
 
+    // Function to assign a new manager
     function addManager(address newManager) public onlyOwner {
-        managers.push(newManager);
+        managers[newManager] = true;
     }
 
+    // Function to transfer the ownership of this contract 
     function transferOwnership(address newOwner) public onlyOwner {
         owner = newOwner;
     }
 
+    // Function to revoke manager access from anyone
     function deleteManager(address oldManager) public onlyOwner {
-        for(uint i; i<managers.length; i++){
-            if(managers[i] == oldManager){
-                delete managers[i];
-                break;
-            }
-        }
+        managers[oldManager] = false;
     }
 
 
