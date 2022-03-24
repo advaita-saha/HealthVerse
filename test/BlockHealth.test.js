72f41c2213b56afb48711c5525f4142effec2f53
Advaita Saha <adv11sphs@gmail.com>
2022-03-24 14:37:13 +0530

    tests added

--- test/BlockHealth.test.js
+++ test/BlockHealth.test.js
@@ -2,15 +2,21 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 let contract;
+let accounts;
 
 beforeEach( async () => {
+    accounts = await ethers.getSigners();
     const BlockHealth = await ethers.getContractFactory("BlockHealth");
     contract = await BlockHealth.deploy();
     await contract.deployed();
 } );
 
 describe("BlockHealth Contract Test", () => {
-    it("Deployment Test and About Us", async function () {
+    it("Deployment Test and About Us", async () => {
         expect(await contract.aboutUs()).to.equal("Secure decentralized electronic health records sharing system based on blockchains");
       });
+
+    it("Owner Check", async () => {
+        expect(await contract.owner()).to.equal(accounts[0].address);
+    });
 });
