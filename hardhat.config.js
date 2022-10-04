099adf20467d7cc22e9ba1447ce6bc0420a8cf08
Advaita Saha <adv11sphs@gmail.com>
2022-10-04 17:53:43 +0530

    j

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,14 +3,4 @@ require('dotenv').config();
 
 module.exports = {
   solidity: "0.8.4",
-  networks : {
-    rinkeby : {
-      url : process.env.RINKEBY,
-      accounts : [process.env.PVT_KEY],
-    },
-    mumbai : {
-      url : process.env.MUMBAI,
-      accounts : [process.env.PVT_KEY],
-    }
-  },
 };
